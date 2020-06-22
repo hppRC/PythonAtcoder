02415818f3db0e81cd71f892ad7f897043c8dc57
hppRC <hpp.ricecake@gmail.com>
2020-06-22 16:00:41 +0900

    chore

--- abc171/f/main.py
+++ abc171/f/main.py
@@ -40,26 +40,53 @@ def LSR(n):
 
 
 sys.setrecursionlimit(1000000)
+MOD = 1000000007
 
-MOD = 10**9+7
+
+class FactInv:
+    def __init__(self, N, MOD=1000000007):
+        fact = [1]*(N+1)
+        for i in range(1, N+1):
+            fact[i] = fact[i - 1] * i % MOD
+
+        inv = [None]*(N+1)
+        inv[N] = pow(fact[N], MOD - 2, MOD)
+
+        for i in range(N)[::-1]:
+            inv[i] = inv[i + 1] * (i + 1) % MOD
+
+        self.N = N
+        self.MOD = MOD
+        self.fact = fact
+        self.inv = inv
+
+    def perm(self, a, b):
+        if a > self.N or b > self.N:
+            raise ValueError(
+                "\nPermutaion arguments are bigger than N\n N = {}, a = {}, b = {}".format(self.N, a, b))
+        return self.fact[a] * self.inv[a-b] % self.MOD
+
+    def comb(self, a, b):
+        if a > self.N or b > self.N:
+            raise ValueError(
+                "\nCombination arguments are bigger than N\n N = {}, a = {}, b = {}".format(self.N, a, b))
+        return self.fact[a] * self.inv[b] * self.inv[a-b] % self.MOD
 
 
 def main():
     K = I()
     l = len(S())
-    ans = 1
 
-    for _ in range(K):
-        ans *= (25 * l + 26)
-        print(ans)
-        l += 1
+    fi = FactInv(K + l + 1)
 
-    tmp = 1
-    for i in range(1, l - 2 - 1):
-        tmp *= i
+    ans = 0
 
-    print(tmp)
-    print((ans // tmp) % MOD)
+    for x in range(K+1):
+        ans += fi.comb(l + K - x - 1, l - 1) * \
+            pow(25, K - x, MOD) * pow(26, x, MOD) % MOD
+        ans %= MOD
+
+    print(ans)
 
 
 main()
