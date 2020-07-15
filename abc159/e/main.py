e29b0ff996aadb97621b931fab38ae38aa4f2389
hppRC <hpp.ricecake@gmail.com>
2020-07-15 13:40:38 +0900

    🥺chore

--- abc159/e/main.py
+++ abc159/e/main.py
@@ -19,8 +19,61 @@ dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
+def bit_pattern(bits, length):
+    return [(bits >> i) & 1 for i in range(length)]
+
 def main():
-    N = I()
+    H, W, K = LI()
+    s = SR(H)
+
+    ans = float("inf")
+
+    for i in range(1 << (H - 1)):
+        pattern = bit_pattern(i, H-1)
+        divided = []
+        tmp = [s[0]]
+        tmp_ans = 0
+
+        for i, bit in enumerate(pattern):
+            if bit:
+                tmp_ans += 1
+                divided.append(tmp)
+                tmp = [s[i+1]]
+            else:
+                tmp.append(s[i+1])
+        divided.append(tmp)
+
+        whites = []
+
+        for block in divided:
+            white = [0] * W
+            for row in block:
+                for i, panel in enumerate(row):
+                    if panel == "1":
+                        white[i] += 1
+            whites.append(white)
+
+        curs = [0] * len(whites)
+        outer = False
+        for w in range(W):
+            for i, white in enumerate(whites):
+                if white[w] > K:
+                    outer = True
+                    break
+                elif white[w] + curs[i] > K:
+                    curs = [white[w] for white in whites]
+                    tmp_ans += 1
+                    break
+                else:
+                    curs[i] += white[w]
+            else:
+                continue
+            if outer:
+                break
+        else:
+            ans = min(ans, tmp_ans)
+
+    print(ans)
 
 if __name__ == '__main__':
     main()
