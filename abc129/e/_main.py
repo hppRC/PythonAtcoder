812f59adf221c4474c0c9fe29b22ff7a495930c4
hppRC <hpp.ricecake@gmail.com>
2020-07-19 08:38:43 +0900

    🥺chore

--- abc129/e/_main.py
+++ abc129/e/_main.py
@@ -19,7 +19,9 @@ def SR(n): return [S() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
 
 class ModInt:
-    def __init__(self, x, MOD=1000000007): self.x, self.MOD = x % MOD, MOD
+    def __init__(self, x, MOD=1000000007):
+        self.x = x % MOD
+        self.MOD = MOD
     def __str__(self): return str(self.x)
     def __add__(self, other): return ModInt(self.x + other.x) if isinstance(other, ModInt) else ModInt(self.x + other)
     def __sub__(self, other): return ModInt(self.x - other.x) if isinstance(other, ModInt) else ModInt(self.x - other)
@@ -29,7 +31,11 @@ class ModInt:
     def __rsub__(self, other): return ModInt(other.x - self.x) if isinstance(other, ModInt) else ModInt(other - self.x)
     def __rtruediv__(self, other): return ModInt(other.x * other.inverse()) if isinstance(other, ModInt) else ModInt(other * pow(self.x, self.MOD - 2, self.MOD))
     def __rpow__(self, other): return ModInt(pow(other.x, self.x, self.MOD)) if isinstance(other, ModInt) else ModInt(pow(other, self.x, self.MOD))
-    __repr__, __radd__, __rmul__ = __str__, __add__, __mul__
+
+    __repr__ = __str__
+    __radd__ = __add__
+    __rmul__ = __mul__
+
     def inverse(self): return pow(self.x, self.MOD - 2, self.MOD)
 
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
@@ -39,20 +45,22 @@ MOD = 1000000007
 def main():
     L = S()
 
-    dp1 = [ModInt(0) for _ in  range(len(L) + 1)]
-    dp2 = [ModInt(0) for _ in  range(len(L) + 1)]
-    dp1[0] = ModInt(1)
+    ans = ModInt(0, MOD)
+    bit_count = 0
 
     for i, bit in enumerate(L):
-        if bit == "0":
-            dp1[i+1] = dp1[i]
-            dp2[i+1] = 3 * dp2[i]
-        else:
-            dp1[i+1] = 2 * dp1[i]
-            dp2[i+1] = 3 * dp2[i] + dp1[i]
-    
-    print(dp1[-1] + dp2[-1])
+        if bit == '0':
+            continue
 
+        d = len(L) - (i + 1)
+        ans += pow(2, bit_count, MOD) * pow(3, d, MOD)
+
+        bit_count += 1
+
+
+    ans += pow(2, bit_count, MOD)
+
+    print(ans)
 
 
 if __name__ == '__main__':
--- snipetts/modint.py
+++ snipetts/modint.py
@@ -1,67 +1,13 @@
 class ModInt:
-    def __init__(self, x):
-        self.x = x % MOD
-
-    def __str__(self):
-        return str(self.x)
-
-    __repr__ = __str__
-
-    def __add__(self, other):
-        return (
-            ModInt(self.x + other.x) if isinstance(other, ModInt) else
-            ModInt(self.x + other)
-        )
-
-    def __sub__(self, other):
-        return (
-            ModInt(self.x - other.x) if isinstance(other, ModInt) else
-            ModInt(self.x - other)
-        )
-
-    def __mul__(self, other):
-        return (
-            ModInt(self.x * other.x) if isinstance(other, ModInt) else
-            ModInt(self.x * other)
-        )
-
-    def __truediv__(self, other):
-        return (
-            ModInt(
-                self.x * pow(other.x, MOD - 2, MOD)
-            ) if isinstance(other, ModInt) else
-            ModInt(self.x * pow(other, MOD - 2, MOD))
-        )
-
-    def __pow__(self, other):
-        return (
-            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
-            ModInt(pow(self.x, other, MOD))
-        )
-
-    __radd__ = __add__
-
-    def __rsub__(self, other):
-        return (
-            ModInt(other.x - self.x) if isinstance(other, ModInt) else
-            ModInt(other - self.x)
-        )
-
-    __rmul__ = __mul__
-
-    def __rtruediv__(self, other):
-        return (
-            ModInt(
-                other.x * pow(self.x, MOD - 2, MOD)
-            ) if isinstance(other, ModInt) else
-            ModInt(other * pow(self.x, MOD - 2, MOD))
-        )
-
-    def __rpow__(self, other):
-        return (
-            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
-            ModInt(pow(other, self.x, MOD))
-        )
-
-    def inverse(self):
-        return pow(self.x, MOD - 2, MOD)
+    def __init__(self, x, MOD=1000000007): self.x, self.MOD = x % MOD, MOD
+    def __str__(self): return str(self.x)
+    def __add__(self, other): return ModInt(self.x + other.x) if isinstance(other, ModInt) else ModInt(self.x + other)
+    def __sub__(self, other): return ModInt(self.x - other.x) if isinstance(other, ModInt) else ModInt(self.x - other)
+    def __mul__(self, other): return ModInt(self.x * other.x) if isinstance(other, ModInt) else ModInt(self.x * other)
+    def __truediv__(self, other): return ModInt(self.x * other.inverse()) if isinstance(other, ModInt) else ModInt(self.x * pow(other, self.MOD - 2, self.MOD))
+    def __pow__(self, other): return ModInt(pow(self.x, other.x, self.MOD)) if isinstance(other, ModInt) else ModInt(pow(self.x, other, self.MOD))
+    def __rsub__(self, other): return ModInt(other.x - self.x) if isinstance(other, ModInt) else ModInt(other - self.x)
+    def __rtruediv__(self, other): return ModInt(other.x * other.inverse()) if isinstance(other, ModInt) else ModInt(other * pow(self.x, self.MOD - 2, self.MOD))
+    def __rpow__(self, other): return ModInt(pow(other.x, self.x, self.MOD)) if isinstance(other, ModInt) else ModInt(pow(other, self.x, self.MOD))
+    __repr__, __radd__, __rmul__ = __str__, __add__, __mul__
+    def inverse(self): return pow(self.x, self.MOD - 2, self.MOD)
