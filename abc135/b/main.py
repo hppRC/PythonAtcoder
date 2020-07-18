2c8153aa105411fd53d6c722475bc4c6d7ec6e21
hppRC <hpp.ricecake@gmail.com>
2020-07-18 17:37:56 +0900

    🥺chore

--- abc135/b/main.py
+++ abc135/b/main.py
@@ -5,25 +5,25 @@ import collections, heapq, itertools, bisect
 import math, fractions
 import sys, copy
 
-sys.setrecursionlimit(1000000)
-
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
 def I(): return int(sys.stdin.readline().rstrip())
 def LS(): return [list(x) for x in sys.stdin.readline().split()]
 def S(): return list(sys.stdin.readline().rstrip())
-def IR(n): return [I() for _ in range(n)]
-def LIR(n): return [LI() for _ in range(n)]
-def LIR1(n): return [LI1() for _ in range(n)]
-def SR(n): return [S() for _ in range(n)]
-def LSR(n): return [LS() for _ in range(n)]
+def IR(n): return [I() for i in range(n)]
+def LIR(n): return [LI() for i in range(n)]
+def LIR1(n): return [LI1() for i in range(n)]
+def SR(n): return [S() for i in range(n)]
+def LSR(n): return [LS() for i in range(n)]
 
+sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
 def main():
     N = I()
+
 
 if __name__ == '__main__':
     main()
--- abc135/d/main.py
+++ abc135/d/main.py
@@ -5,8 +5,6 @@ import collections, heapq, itertools, bisect
 import math, fractions
 import sys, copy
 
-sys.setrecursionlimit(1000000)
-
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
 def I(): return int(sys.stdin.readline().rstrip())
@@ -18,12 +16,31 @@ def LIR1(n): return [LI1() for _ in range(n)]
 def SR(n): return [S() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
 
+sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
 def main():
-    N = I()
+    s = S()
+
+    dp = [[0] * 13 for _ in range(len(s) + 1)]
+    dp[0][0] = 1
+
+    for i in range(len(s)):
+        if s[i] == "?":
+            for ch in range(10):
+                for j in range(13):
+                    dp[i+1][(j * 10 + ch) % 13] += dp[i][j]
+        else:
+            ch = int(s[i])
+            for j in range(13):
+                dp[i+1][(j * 10 + ch) % 13] += dp[i][j]
+
+        for j in range(13):
+            dp[i+1][j] %= MOD
+
+    print(dp[len(s)][5])
 
 if __name__ == '__main__':
     main()
