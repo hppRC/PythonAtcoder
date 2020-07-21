828cf3ee8c342afcb713e7c52868ba1691ed66b8
hppRC <hpp.ricecake@gmail.com>
2020-07-21 21:05:54 +0900

    🥺chore

--- abc160/c/main.py
+++ abc160/c/main.py
@@ -1,31 +1,37 @@
-#!usr/bin/env pypy3
-from collections import defaultdict, deque
-from heapq import heappush, heappop
-from itertools import permutations, accumulate
-import sys
-import math
-import bisect
-def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def I(): return int(sys.stdin.readline())
-def LS():return [list(x) for x in sys.stdin.readline().split()]
-def S():
-    res = list(sys.stdin.readline())
-    if res[-1] == "\n":
-        return res[:-1]
-    return res
-def IR(n):
-    return [I() for i in range(n)]
-def LIR(n):
-    return [LI() for i in range(n)]
-def SR(n):
-    return [S() for i in range(n)]
-def LSR(n):
-    return [LS() for i in range(n)]
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from functools import reduce, lru_cache
+import collections, heapq, itertools, bisect
+import math, fractions
+import sys, copy
 
 sys.setrecursionlimit(1000000)
 
-def main():
-    s = input()
-    print(s)
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def I(): return int(sys.stdin.readline().rstrip())
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def S(): return list(sys.stdin.readline().rstrip())
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
 
-main()
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
+
+def main():
+    K, N = LI()
+    A = LI()
+
+    res = [A[0] + K - A[-1]]
+    for i in range(N-1):
+        res.append(A[i+1] - A[i])
+    res = sorted(res)
+
+    print(K - res[-1])
+
+if __name__ == '__main__':
+    main()
