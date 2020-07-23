5c240c943e1103b33ab6cfd8ca6e693e15d4df59
hppRC <hpp.ricecake@gmail.com>
2020-07-23 23:12:48 +0900

    🥺chore

--- abc158/d/main.py
+++ abc158/d/main.py
@@ -5,29 +5,49 @@ import collections, heapq, itertools, bisect
 import math, fractions
 import sys, copy
 
-sys.setrecursionlimit(1000000)
-
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
-def I(): return int(sys.stdin.readline().rstrip())
 def LS(): return [list(x) for x in sys.stdin.readline().split()]
-def S(): return list(sys.stdin.readline().rstrip())
 def IR(n): return [I() for _ in range(n)]
 def LIR(n): return [LI() for _ in range(n)]
 def LIR1(n): return [LI1() for _ in range(n)]
 def SR(n): return [S() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
 
+sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
 def main():
-    N, M = LI()
-    A = LI()
-    s = sum(A)
+    s = S()
+    q = I()
+    Q = LR(q)
 
-    print("Yes" if len(list(filter(lambda x: 4 * M * x >= s, A))) >= M else "No")
+    turned = False
+    q = deque()
+    for c in s:
+        q.append(c)
+
+    for query in Q:
+        if query[0] == "1":
+            turned = not turned
+        else:
+            _, f, c = query
+            if (not turned and f == "1") or (turned and f == "2"):
+                q.appendleft(c)
+            else:
+                q.append(c)
+
+
+    if turned:
+        q.reverse()
+    ans = "".join(q)
+    print(ans)
 
 
 if __name__ == '__main__':
