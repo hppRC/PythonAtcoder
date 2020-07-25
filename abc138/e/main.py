da1e54bcbac27edaebd043e24d85a8dc0884bffa
hppRC <hpp.ricecake@gmail.com>
2020-07-25 20:30:56 +0900

    🥺chore

--- abc138/e/main.py
+++ abc138/e/main.py
@@ -1,9 +1,11 @@
 #!usr/bin/env python3
-from collections import defaultdict, deque, Counter, OrderedDict
-from functools import reduce, lru_cache
 import collections, heapq, itertools, bisect
 import math, fractions
 import sys, copy
+
+from collections import defaultdict, deque, Counter, OrderedDict
+from functools import reduce, lru_cache
+from bisect import bisect_left, bisect_right
 
 def L(): return sys.stdin.readline().split()
 def I(): return int(sys.stdin.readline().rstrip())
@@ -18,13 +20,40 @@ def SR(n): return [S() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
 def LR(n): return [L() for _ in range(n)]
 
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+
 sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
 def main():
-    N = I()
+    s = S()
+    t = S()
+
+    chars = {ch: [] for ch in alphabets}
+
+    for i, ch in enumerate(s):
+        chars[ch].append(i)
+
+    ans = 0
+    last = -1
+    for ch in t:
+        if len(chars[ch]) == 0:
+            print(-1)
+            return
+
+        idx = bisect_right(chars[ch], last)
+        idx = idx if idx < len(chars[ch]) else 0
+        if chars[ch][idx] > last:
+            ans += chars[ch][idx] - last
+        else:
+            ans += len(s) - last + chars[ch][idx]
+        last = chars[ch][idx]
+
+    print(ans)
+
+
 
 if __name__ == '__main__':
     main()
