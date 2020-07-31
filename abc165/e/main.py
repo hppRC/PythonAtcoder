fea503957c4497022ce470762123d0cfae26beff
hppRC <hpp.ricecake@gmail.com>
2020-07-31 16:51:51 +0900

    🥺chore

--- abc165/e/main.py
+++ abc165/e/main.py
@@ -1,31 +1,49 @@
-#!usr/bin/env pypy3
-from collections import defaultdict, deque
-from heapq import heappush, heappop
-from itertools import permutations, accumulate
-import sys
-import math
-import bisect
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def I(): return int(sys.stdin.readline())
-def LS():return [list(x) for x in sys.stdin.readline().split()]
-def S():
-    res = list(sys.stdin.readline())
-    if res[-1] == "\n":
-        return res[:-1]
-    return res
-def IR(n):
-    return [I() for i in range(n)]
-def LIR(n):
-    return [LI() for i in range(n)]
-def SR(n):
-    return [S() for i in range(n)]
-def LSR(n):
-    return [LS() for i in range(n)]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 sys.setrecursionlimit(1000000)
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
 
 def main():
-    s = input()
-    print(s)
+    N, M = LI()
+    if N % 2 == 1:
+        for i in range(1, M+1):
+            print(i, N - i)
+    else:
+        for i in range(1, M + 1):
+            x = i
+            y = N - i + 1
+            if y - x > (x - y) % N:
+                print(x, y)
+            else:
+                print(x, y - 1)
 
-main()
+
+
+if __name__ == '__main__':
+    main()
