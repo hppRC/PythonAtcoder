337f765a7eb7c442c1cef551cbaaf2bf23d9778e
hppRC <hpp.ricecake@gmail.com>
2020-08-01 20:59:07 +0900

    🥺chore

--- abc159/c/main.py
+++ abc159/c/main.py
@@ -1,18 +1,28 @@
 #!usr/bin/env python3
 from collections import defaultdict, deque, Counter, OrderedDict
-import collections, heapq, itertools, bisect
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools
 import math, fractions
 import sys, copy
 
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
-def I(): return int(sys.stdin.readline().rstrip())
 def LS(): return [list(x) for x in sys.stdin.readline().split()]
-def S(): return list(sys.stdin.readline().rstrip())
-def IR(n): return [I() for i in range(n)]
-def LIR(n): return [LI() for i in range(n)]
-def SR(n): return [S() for i in range(n)]
-def LSR(n): return [LS() for i in range(n)]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
@@ -20,7 +30,8 @@ dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 MOD = 1000000007
 
 def main():
-    N = I()
+    L = I()
+    print(L ** 3 / 27)
 
 if __name__ == '__main__':
     main()
