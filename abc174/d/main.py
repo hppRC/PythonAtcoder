a00b5a0aca2660dd0640c559d50d176449ba28be
hppRC <hpp.ricecake@gmail.com>
2020-08-03 12:23:02 +0900

    🥺chore

---
+++ abc174/d/main.py
@@ -0,0 +1,55 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools, bisect
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+sys.setrecursionlimit(1000000)
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
+
+def main():
+    N = I()
+    C = S()
+
+    wi, ri = 0, N-1
+    ans = 0
+
+    while ri >= 0 and wi < N:
+        while wi < N and C[wi] != "W":
+            wi += 1
+        while ri >= 0 and C[ri] != "R":
+            ri -= 1
+        if wi >= ri:
+            break
+        if wi < N and ri >= 0:
+            C[wi], C[ri] = C[ri], C[wi]
+        wi += 1
+        ri -= 1
+        ans += 1
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
---
+++ abc174/e/main.py
@@ -0,0 +1,60 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools, bisect
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+sys.setrecursionlimit(1000000)
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
+
+def main():
+    N, K = LI()
+    A = LI()
+
+    maxA = max(A)
+    ans = maxA
+
+    left, right = 1, maxA
+    mid = (left + right) / 2
+    e = 0.000001
+
+    while right - left > e:
+        count = 0
+        for ai in A:
+            count += math.floor(ai / mid)
+        if count <= K:
+            right = mid
+            ans = min(mid, ans)
+        else:
+            left = mid
+        mid = (left + right) / 2
+
+    if math.floor(ans) + e > ans:
+        print(math.floor(ans))
+    else:
+        print(math.ceil(ans))
+
+if __name__ == '__main__':
+    main()
