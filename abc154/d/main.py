1639906c9c83cf14ff6da65c6dbe38ad3b118a17
hppRC <hpp.ricecake@gmail.com>
2020-08-04 16:39:39 +0900

    🥺chore

---
+++ abc154/d/main.py
@@ -0,0 +1,47 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools, bisect
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def S(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+sys.setrecursionlimit(1000000)
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
+
+def main():
+    N, K = LI()
+    P = LI()
+
+    cum = 0
+    for i in range(K):
+        cum += (P[i] + 1) / 2
+
+    ans = cum
+    for i in range(N - K):
+        cum += (P[i + K] + 1) / 2 - (P[i] + 1) / 2
+        ans = max(ans, cum)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
