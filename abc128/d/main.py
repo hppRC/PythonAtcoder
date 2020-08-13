c6bcb8448f98f5e139cc0e50a414edd787e0ce39
hppRC <hpp.ricecake@gmail.com>
2020-08-13 13:44:43 +0900

    🥺chore

---
+++ abc128/d/main.py
@@ -0,0 +1,56 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools, bisect
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def SL(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+
+def perm(n, r): return math.factorial(n) // math.factorial(r)
+def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+sys.setrecursionlimit(1000000)
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+MOD = 1000000007
+INF = float("inf")
+
+def main():
+    N, K = LI()
+    V = LI()
+    V2 = V + V
+
+    ans = 0
+    for k in range(1, min(K + 1, N + 1)):
+        for i in range(len(V) - k, len(V) + 1):
+            neg = []
+            tmp = 0
+            for vi in V2[i:i + k]:
+                if vi < 0:
+                    neg.append(vi)
+                tmp += vi
+            tmp -= sum(sorted(neg)[:K - k])
+            ans = max(ans, tmp)
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
