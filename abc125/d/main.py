29d4c69d839d4b13dce81499d294993ba78bf339
hppRC <hpp.ricecake@gmail.com>
2020-08-17 17:35:54 +0900

    🥺chore

--- abc125/d/main.py
+++ abc125/d/main.py
@@ -4,7 +4,7 @@ from bisect import bisect_left, bisect_right
 from functools import reduce, lru_cache
 from heapq import heappush, heappop, heapify
 
-import itertools, bisect
+import itertools
 import math, fractions
 import sys, copy
 
@@ -17,12 +17,14 @@ def LS(): return [list(x) for x in sys.stdin.readline().split()]
 def IR(n): return [I() for _ in range(n)]
 def LIR(n): return [LI() for _ in range(n)]
 def LIR1(n): return [LI1() for _ in range(n)]
-def SR(n): return [S() for _ in range(n)]
+def SR(n): return [SL() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
 def LR(n): return [L() for _ in range(n)]
 
 def perm(n, r): return math.factorial(n) // math.factorial(r)
 def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+
+def make_list(n, *args, default=0): return [make_table(*args, default=default) for _ in range(n)] if len(args) > 0 else [default for _ in range(n)]
 
 alphabets = "abcdefghijklmnopqrstuvwxyz"
 ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -35,6 +37,14 @@ INF = float("inf")
 
 def main():
     N = I()
+    A = LI()
+    cnt = sum([1 for ai in A if ai < 0])
+    B = [abs(ai) for ai in A]
+    if cnt % 2 == 0:
+        print(sum(B))
+    else:
+        print(sum(B) - min(B) * 2)
+
 
 if __name__ == '__main__':
     main()
