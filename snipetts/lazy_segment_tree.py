41c82e39eabe230bebfbfc99b67a758b5baf65b3
hppRC <hpp.ricecake@gmail.com>
2020-08-19 13:00:33 +0900

    🥺chore

--- snipetts/lazy_segment_tree.py
+++ snipetts/lazy_segment_tree.py
@@ -1,40 +1,4 @@
-#!usr/bin/env python3
-from collections import defaultdict, deque, Counter, OrderedDict
-from bisect import bisect_left, bisect_right
-from functools import reduce, lru_cache
-from heapq import heappush, heappop, heapify
-
-import itertools
-import math, fractions
-import sys, copy
-
-def L(): return sys.stdin.readline().split()
-def I(): return int(sys.stdin.readline().rstrip())
-def SL(): return list(sys.stdin.readline().rstrip())
-def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
-def LS(): return [list(x) for x in sys.stdin.readline().split()]
-def IR(n): return [I() for _ in range(n)]
-def LIR(n): return [LI() for _ in range(n)]
-def LIR1(n): return [LI1() for _ in range(n)]
-def SR(n): return [SL() for _ in range(n)]
-def LSR(n): return [LS() for _ in range(n)]
-def LR(n): return [L() for _ in range(n)]
-
-def perm(n, r): return math.factorial(n) // math.factorial(r)
-def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
-
-def make_list(n, *args, default=0): return [make_table(*args, default=default) for _ in range(n)] if len(args) > 0 else [default for _ in range(n)]
-
-alphabets = "abcdefghijklmnopqrstuvwxyz"
-ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-sys.setrecursionlimit(1000000)
-dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
-MOD = 1000000007
-INF = float("inf")
-
+# still buggy
 # https://tjkendev.github.io/procon-library/python/range_query/rmq_ruq_segment_tree_lp.html
 class LazySegmentTree:
     # N: 処理する区間の長さ
@@ -130,41 +94,3 @@ class LazySegmentTree:
             L >>= 1
             R >>= 1
         return s
-
-def main():
-    N = 10
-
-    seg = LazySegmentTree(N)
-    seg.update(5, 0, 10)
-    seg.affect(5, 0, 5)
-    seg.affect(50, 0, 5)
-    seg.affect(100, 6)
-    seg.update(0, 2, 4)
-    seg.affect(1, 0, 2)
-
-    for i in range(N):
-        print(seg.query(i), end=" ")
-    print()
-
-    print(seg.query(0, 3))
-    print(seg.query(0, 6))
-    print(seg.query(0, 10))
-    print(seg.query(5, 10))
-
-    seg.update(-10, 6)
-    seg.affect(-200, 0, 3)
-
-    for i in range(N):
-        print(seg.query(i), end=" ")
-    print()
-
-    print(seg.query(0, 3))
-    print(seg.query(0, 6))
-    print(seg.query(0, 10))
-    print(seg.query(5, 10))
-
-
-
-
-if __name__ == '__main__':
-    main()
