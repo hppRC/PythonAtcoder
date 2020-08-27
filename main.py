60567c8a258819eda111fa46286fb659149910a2
hppRC <hpp.ricecake@gmail.com>
2020-08-27 10:42:23 +0900

    🥺chore

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def SL(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
 def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def R(n): return [sys.stdin.readline().strip() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
 def IR(n): return [I() for _ in range(n)]
 def LIR(n): return [LI() for _ in range(n)]
 def LIR1(n): return [LI1() for _ in range(n)]
 def SR(n): return [SL() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
-def LR(n): return [L() for _ in range(n)]
 
 def perm(n, r): return math.factorial(n) // math.factorial(r)
 def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
