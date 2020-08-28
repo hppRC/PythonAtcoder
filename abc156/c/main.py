a4a5974ed46d825dd23b8052bd37dabaf7319fa5
hppRC <hpp.ricecake@gmail.com>
2020-08-28 11:56:56 +0900

    🥺chore

--- abc156/c/main.py
+++ abc156/c/main.py
@@ -1,31 +1,51 @@
-#!usr/bin/env pypy3
-from collections import defaultdict, deque
-from heapq import heappush, heappop
-from itertools import permutations, accumulate
-import sys
-import math
-import bisect
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def SL(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
-def I(): return int(sys.stdin.readline())
-def LS():return [list(x) for x in sys.stdin.readline().split()]
-def S():
-    res = list(sys.stdin.readline())
-    if res[-1] == "\n":
-        return res[:-1]
-    return res
-def IR(n):
-    return [I() for i in range(n)]
-def LIR(n):
-    return [LI() for i in range(n)]
-def SR(n):
-    return [S() for i in range(n)]
-def LSR(n):
-    return [LS() for i in range(n)]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def R(n): return [sys.stdin.readline().strip() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SR(n): return [SL() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+
+def perm(n, r): return math.factorial(n) // math.factorial(r)
+def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+
+def make_list(n, *args, default=0): return [make_list(*args, default=default) for _ in range(n)] if len(args) > 0 else [default for _ in range(n)]
 
 sys.setrecursionlimit(1000000)
+dire, dire8 = [[1, 0], [0, 1], [-1, 0], [0, -1]], [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+alphabets, ALPHABETS = "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+MOD, INF = 1000000007, float("inf")
 
 def main():
-    s = input()
-    print(s)
+    N = I()
+    X = LI()
 
-main()
+    ans = INF
+
+    for p in range(1, 101):
+        tmp = 0
+        for x in X:
+            tmp += (x - p) ** 2
+        ans = min(ans, tmp)
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
