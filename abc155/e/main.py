dee23cb0a9a1e1d535aa686a0a8afa69b3c0cd48
hppRC <hpp.ricecake@gmail.com>
2020-09-09 16:55:29 +0900

    🥺chore

--- abc155/e/main.py
+++ abc155/e/main.py
@@ -4,33 +4,52 @@ from bisect import bisect_left, bisect_right
 from functools import reduce, lru_cache
 from heapq import heappush, heappop, heapify
 
-import itertools, bisect
+import itertools
 import math, fractions
 import sys, copy
 
 def L(): return sys.stdin.readline().split()
 def I(): return int(sys.stdin.readline().rstrip())
-def S(): return list(sys.stdin.readline().rstrip())
+def SL(): return list(sys.stdin.readline().rstrip())
 def LI(): return [int(x) for x in sys.stdin.readline().split()]
 def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
 def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def R(n): return [sys.stdin.readline().strip() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
 def IR(n): return [I() for _ in range(n)]
 def LIR(n): return [LI() for _ in range(n)]
 def LIR1(n): return [LI1() for _ in range(n)]
-def SR(n): return [S() for _ in range(n)]
+def SR(n): return [SL() for _ in range(n)]
 def LSR(n): return [LS() for _ in range(n)]
-def LR(n): return [L() for _ in range(n)]
 
-alphabets = "abcdefghijklmnopqrstuvwxyz"
-ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def perm(n, r): return math.factorial(n) // math.factorial(r)
+def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
 
-sys.setrecursionlimit(1000000)
+def make_list(n, *args, default=0): return [make_list(*args, default=default) for _ in range(n)] if len(args) > 0 else [default for _ in range(n)]
+
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 MOD = 1000000007
+INF = float("inf")
+
+sys.setrecursionlimit(1000000)
 
 def main():
-    N = I()
+    N = SL()[::-1]
+    dp = [0] * (len(N) + 1)
+
+    for i, e in enumerate(N):
+        if int(e) + dp[i] >= 6:
+            dp[i] = 10 - (int(e) + dp[i])
+            dp[i+1] += 1
+        elif int(e) + dp[i] == 5 and i < len(N)-1 and int(N[i+1]) >= 5:
+            dp[i] = 5
+            dp[i+1] += 1
+        else:
+            dp[i] += int(e)
+    print(sum(dp))
 
 if __name__ == '__main__':
     main()
