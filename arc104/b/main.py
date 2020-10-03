12f83e6660efaafc37710a652921301d9dbc77b7
hppRC <hpp.ricecake@gmail.com>
2020-10-04 01:22:02 +0900

    🥺chore

---
+++ arc104/b/main.py
@@ -0,0 +1,58 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+import itertools
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def SL(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def R(n): return [sys.stdin.readline().strip() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SLR(n): return [SL() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+
+def perm(n, r): return math.factorial(n) // math.factorial(r)
+def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+
+def make_list(n, *args, default=0): return [make_list(*args, default=default) for _ in range(n)] if args else [default for _ in range(n)]
+
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+MOD = 1000000007
+INF = float("inf")
+
+sys.setrecursionlimit(1000000)
+
+def main():
+    N, S = L()
+    N = int(N)
+
+    d = "ATCG"
+    cum = make_list(4, N+1)
+    for i, ch in enumerate(S):
+        for j in range(4):
+            cum[j][i+1] = cum[j][i] + int(ch == d[j])
+
+    ans = 0
+    for l in range(N+1):
+        for r in range(l+1, N+1):
+            if (cum[0][r] - cum[0][l] == cum[1][r] - cum[1][l]) and (cum[2][r] - cum[2][l] == cum[3][r] - cum[3][l]):
+                ans += 1
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
