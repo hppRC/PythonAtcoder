9e46daa3f0c84b5fa6a6b37011614b1fe2c1ca29
hppRC <hpp.ricecake@gmail.com>
2020-11-07 21:28:51 +0900

    🥺chore

--- abc129/c/main.py
+++ abc129/c/main.py
@@ -17,10 +17,23 @@ def LSR(n): return [LS() for i in range(n)]
 sys.setrecursionlimit(1000000)
 dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
-MOD = 1000000007
+MOD = 1_000_000_007
 
 def main():
-    N = I()
+    N, M = LI()
+    A = set(IR(M))
+
+    dp = [0]*(N+1)
+    dp[0] = 1
+    dp[1] = 1 if 1 not in A else 0
+
+    for i in range(2, N+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % MOD
+        if i in A:
+            dp[i] = 0
+    print(dp[-1])
+
+
 
 if __name__ == '__main__':
     main()
