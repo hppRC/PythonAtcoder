5fd60427c1c08e0f18ed5f65c99e396a8bf4e917
hppRC <hpp.ricecake@gmail.com>
2020-11-18 15:09:47 +0900

    🥺chore

---
+++ abc111/c/main.py
@@ -0,0 +1,64 @@
+#!usr/bin/env python3
+from collections import defaultdict, deque, Counter, OrderedDict
+from bisect import bisect_left, bisect_right
+from functools import reduce, lru_cache
+from heapq import heappush, heappop, heapify
+
+from itertools import *
+import math, fractions
+import sys, copy
+
+def L(): return sys.stdin.readline().split()
+def I(): return int(sys.stdin.readline().rstrip())
+def SL(): return list(sys.stdin.readline().rstrip())
+def LI(): return [int(x) for x in sys.stdin.readline().split()]
+def LI1(): return [int(x) - 1 for x in sys.stdin.readline().split()]
+def LS(): return [list(x) for x in sys.stdin.readline().split()]
+def R(n): return [sys.stdin.readline().strip() for _ in range(n)]
+def LR(n): return [L() for _ in range(n)]
+def IR(n): return [I() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def LIR1(n): return [LI1() for _ in range(n)]
+def SLR(n): return [SL() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+
+def perm(n, r): return math.factorial(n) // math.factorial(r)
+def comb(n, r): return math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def make_list(n, *args, default=0): return [make_list(*args, default=default) for _ in range(n)] if args else [default for _ in range(n)]
+
+dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+dire8 = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+MOD = 1000000007
+INF = float("inf")
+
+sys.setrecursionlimit(1000000)
+
+def main():
+    n = I()
+    V = LI()
+    c1 = Counter(V[::2])
+    c2 = Counter(V[1::2])
+
+    (r1, d1), (r2, d2) = c1.most_common(1)[0], c2.most_common(1)[0]
+    if r1 == r2:
+        w1, w2 = c1.most_common(2), c2.most_common(2)
+        (r3, d3), (r4, d4) = w1[-1], w2[-1]
+        if len(w1) >= 2 and len(w2) >= 2:
+            print(min(n - d3 - d2, n - d1 - d4))
+        elif len(w1) == 1 and len(w2) >= 2:
+            print(n - d1 - d4)
+        elif len(w1) >= 2 and len(w2) == 1:
+            print(n - d2 - d3)
+        else:
+            print(n - d1)
+    else:
+        print(n - d1 - d2)
+
+if __name__ == '__main__':
+    main()
